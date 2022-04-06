20616eb58d0fc237aa4f0217347ebb10c71b6b35
Swepool <36674091+Swepool@users.noreply.github.com>
2022-04-06 16:38:57 +0200

    Global font settings

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -56,15 +56,6 @@
 </div>
 
 <style>
-    :root {
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-        Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-
-    :global(body) {
-        margin: 0;
-        padding: 0;
-    }
 
     .wrapper {
         display: flex;
