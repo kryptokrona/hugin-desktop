dcb54a0fb7d6a08731a03fbb8125c8b46f0aed5f
n9lsjr <rasmus@lingonbyran.se>
2022-05-04 12:23:05 +0200

    Some dashboard html for profile and set huginAddr from store.

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -1,6 +1,14 @@
 <script>
     import { fade } from 'svelte/transition';
     import Balance from "/src/components/finance/Balance.svelte";
+    import {user} from "$lib/stores/user.js";
+    import {get_avatar} from "$lib/utils/hugin-utils.js";
+
+    console.log('myAddress',$user.huginAddress);
+    const myAddress = $user.huginAddress
+    let address = myAddress.substring(0,99);
+    let messageKey =  myAddress.substring(99,163);
+    let avatar = get_avatar(address)
 </script>
 
 <main in:fade>
@@ -8,6 +16,40 @@
         <h1>Dashboard</h1>
         <Balance/>
     </div>
+
+    <h1>Welcome back!</h1>
+
+     <div id="profile">
+
+       <h2>Profile</h2>
+       <div class="inner">
+          <div class="dashboard_avatar">
+          <img class="avatar" src="data:image/png;base64,{avatar}" alt="avatar">
+          </div>
+       </div>
+         <table style="width:100%">
+           <br>
+             <div id="headerText" class="inline">
+               <div id="currentAddr" class="inline">
+
+                 <span class="description">Payment address</span>&nbsp;
+                 <br>
+                 <span id="currentAddrSpan">{address}</span>
+                 <br>
+
+                 <span class="description">Message key</span>&nbsp;
+                 <br>
+                 <span id="currentPubKey">{messageKey}</span>
+
+               </div>
+               <button id="profile_copy_address">Copy both</button>
+
+             </div>
+         </table>
+
+       </div>
+
+
 </main>
 
 <style>
@@ -32,6 +74,10 @@
 
     h1 {
         font-weight: 400;
+    }
+
+    .huginAddress {
+
     }
 
 </style>
