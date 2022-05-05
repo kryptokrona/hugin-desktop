22718a28fc5560adf64f756d347a454c10d51558
n9lsjr <rasmus@lingonbyran.se>
2022-05-05 04:55:14 +0200

    Fixed sending and printing board messages

--- src/routes/boards/index.svelte
+++ src/routes/boards/index.svelte
@@ -2,12 +2,58 @@
     import { fade } from 'svelte/transition';
     import ChatWindow from "/src/components/chat/ChatWindow.svelte";
     import ChatInput from "/src/components/chat/ChatInput.svelte";
+    import {boardMessages} from '$lib/stores/boardmsgs.js';
+    import ChatBubble from "/src/components/chat/ChatBubble.svelte";
+    import {user} from '$lib/stores/user.js'
+    import {onMount} from "svelte";
+
+    onMount(async () => {
+
+        //If we have an active chat in store we show that conversation
+            printMessages()
+    })
+
+    $ : boardMsgs = []
+
+    $: {
+        window.api.receive('boardMsg', async (data) => {
+          console.log('boardMsg', data)
+            //Push new message to store
+            //boardMessages.update(() => data.boardMessages)
+            //Update boardsMsgs from store
+            boardMsgs.push(data)
+            boardMsgs = boardMsgs
+            console.log('Boards msgs array?', boardMsgs)
+
+            //console.log('Updated boardmsgs', bMessages);
+            //<ChatBubble message={boardmessage.m} ownMsg={boardmessage.sent} msgFrom={boardmessage.k} board={boardmessage.brd}/>
+        })
+    }
+
+    function printMessages() {
+      boardMsgs = $boardMessages
+    }
+
+    //Send message to store and DB
+    const sendboardMsg = e => {
+      console.log('Sending board msg');
+
+        let msg = e.detail.text
+        console.log(e)
+        window.api.sendBoardMsg(msg, thisBoard)
+    }
+
+    $ : thisBoard = $user.thisBoard
 
 </script>
 
 <main>
     <ChatWindow>
-        <ChatInput/>
+        <ChatInput on:message={sendboardMsg}/>
+                    {#each boardMsgs as message}
+                    <ChatBubble handleType={message.r} message={message.m} myMsg={message.sent} signature={message.s} board={message.brd} nickname={message.n} msgFrom={message.k}/>
+
+                    {/each}
     </ChatWindow>
     <div class="list">
 
@@ -30,5 +76,10 @@
     .list {
         width: 350px;
         border-left: 1px solid rgba(255,255,255,0.1);
+    }
+
+    p {
+      font-size: 17px;
+      color: white;
     }
 </style>
