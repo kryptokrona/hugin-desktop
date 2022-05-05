fe3c00366328a153ffd58368d5d68f33e24684cf
n9lsjr <rasmus@lingonbyran.se>
2022-05-05 15:58:33 +0200

    Added BoardWindow component

---
+++ src/components/chat/BoardWindow.svelte
@@ -0,0 +1,30 @@
+<script>
+    //Don't know if we can handle date here, to print today, yesterday etc in chat.
+import ChatInput from "/src/components/chat/ChatInput.svelte";
+
+</script>
+
+
+
+<!--This is just a wrapper that takes nested components-->
+<div class="window">
+    <slot></slot>
+</div>
+
+<style>
+
+    .window {
+        margin-right: 85px;
+        box-sizing: border-box;
+        background-color: #202020;
+        width: 100%;
+        overflow: scroll;
+        z-index: 3;
+        margin-top: 10%;
+    }
+
+    .window::-webkit-scrollbar {
+    display: none;
+    }
+
+</style>
