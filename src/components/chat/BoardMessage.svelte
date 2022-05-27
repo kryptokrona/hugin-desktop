e679252c3145ef8fb30250ce35eb5be34d3dcbbe
n9lsjr <rasmus@lingonbyran.se>
2022-05-27 12:20:32 +0200

    BoardMessage reply fix and styling

--- src/components/chat/BoardMessage.svelte
+++ src/components/chat/BoardMessage.svelte
@@ -19,7 +19,8 @@
 
           thisreply = await window.api.getReply(reply)
           console.log('thisreply found', thisreply);
-
+          //Add extra number to avoid collision for keys in Svelte each loop
+          thisreply.hash = thisreply.hash + '1337'
           return thisreply
       }
 
@@ -34,9 +35,14 @@
       <p>{thisreply.m}</p>
       </div>
   {:catch error}
-  	<p style="color: red">{error.message}</p>
+  <div class="reply">     <img class="reply_avatar"
+  src="data:image/png;base64,{get_avatar('SEKReU6UELRfBmKNUuo5mP58LVQcQqEKwZgfC7hMd5puRjMLJ5cJcLbFLkJCh6CpsB9WD2z4kqKWQGVABJxRAG5z9Hc1Esg1KV4')}" alt="">
+  <p class="reply_nickname">Can't find reply</p> <br>
+    	<p style="color: red">{error.message}</p>
+      </div>
   {/await}
-  <div class:type={handleType} class="boardMessage reply">
+  <div class="replyline"> </div>
+  <div on:click class:type={handleType} class="boardMessage replyer">
       <img class="avatar"
            src="data:image/png;base64,{get_avatar(msgFrom)}" alt="">
            <p class="nickname">{nickname}</p><br>
@@ -54,40 +60,64 @@
   {/if}
 <style>
     .boardMessage {
-        display: flex;
-        align-items: center;
-        color: rgba(255, 255, 255, 0.8);
-        padding: 15px 25px 15px 15px;
-        border-bottom: 1px solid rgba(255, 255, 255, 0.1);;
-        z-index: 3;
+    display: flex;
+    align-items: center;
+    color: rgba(255, 255, 255, 0.8);
+    padding: 15px 25px 15px 15px;
+    z-index: 3;
+    font-size: 12px;
+    margin-left: 2%;
+    width: 92%;
+    background: rgba(0,0,0,0.2);
+    border-radius: 15px;
+    margin-bottom: 5px;
     }
+
+    .nickname {
+      font-size: 14px;
+      font-weight: bold;
+    }
+
     .type {
         color: rgba(255, 255, 255, 0.8);
     }
+
     p {
         margin: 0;
         word-break: break-word;
         display: contents;
+        font-family: "Montserrat" !important;
+        font-size: 13px;
     }
 
     .reply_avatar {
-        height: 14px;
-        width: 14px;
+        height: 30px;
+        width: 30px;
+    }
+
+    .reply_nickname {
+      font-size: 12px;
+      font-weight: bold;
     }
 
     .reply {
       display: flex;
       align-items: center;
       color: rgba(255, 255, 255, 0.8);
-      padding: 15px 25px 15px 15px;
-      border-bottom: 1px solid rgba(255, 255, 255, 0.1);;
+      padding: 5px 12px 5px 5px;
       z-index: 3;
-      font-size: 14px;
+      font-size: 12px;
+      width: fit-content;
+      opacity: 0.85;
+      border-radius: 10px;
+      margin-left: 4%;
+      background: rgba(0,0,0,0.1);
     }
 
-    .nickname {
-      color: white;
-      font-size: 17px;
-      font-family: "Montserrat";
+    .reply p {
+      font-size: 11px;
+    }
+    .replyer {
+      border-top: none;
     }
 </style>
