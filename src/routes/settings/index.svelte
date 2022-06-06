d7a92cc9b2640e708b2a9b88fe6f06f4cba6f058
n9lsjr <rasmus@lingonbyran.se>
2022-06-06 18:30:39 +0200

    Some node settings test

--- src/routes/settings/index.svelte
+++ src/routes/settings/index.svelte
@@ -1,28 +1,56 @@
 <script>
     import { fade } from 'svelte/transition';
     import {nodelist} from "$lib/stores/nodes.js";
+    import {user} from "$lib/stores/user.js";
     let node;
+
+
+    const switchNode = (node) => {
+
+      console.log('swiitching', node);
+
+      //window.api.switchNode(node)
+
+      user.update(oldData => {
+          return {
+              ...oldData,
+              node: node
+          }
+      })
+
+
+    }
+    $ : console.log('node?', node);
+
 </script>
 
 <main in:fade>
     <h1>Settings</h1>
 
-    <select bind:value={node}>
         {#each $nodelist as node}
-            <option value={`${node.url}:${node.port}`}>{node.name}</option>
+        <div on:click={()=> switchNode(node)}>
+            <h4>{node.name}</h4>
+            <p>{`${node.url}:${node.port}`}</p>
+        </div>
         {/each}
 </main>
 
 <style>
-    h1 {
+    h1,h2,h3,h4 {
         color: white;
-        margin: 0
+        margin: 0;
+        font-family: "Montserrat"
     }
 
     main {
         margin-left: 85px;
         padding: 40px;
         z-index: 3;
+    }
+
+    p {
+      font-family: "Roboto Mono";
+      color: white;
     }
 
 </style>
