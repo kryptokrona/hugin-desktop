38e2754d190af0bb9c0408e011225f81d49c446d
n9lsjr <rasmus@lingonbyran.se>
2022-06-16 02:30:56 +0200

    Login sequence with custom or default node

--- src/routes/create-account/index.svelte
+++ src/routes/create-account/index.svelte
@@ -3,23 +3,30 @@
     import FillButton from "/src/components/buttons/FillButton.svelte";
     import {user} from "$lib/stores/user.js";
     import { goto } from '$app/navigation';
+    import GreenButton from "/src/components/chat/GreenButton.svelte"
+    import {nodelist} from "$lib/stores/nodes.js";
 
-    let password;
-    let walletName;
-    let node;
-    let port
+    let password = ''
+    let walletName = ''
     let nodeInput = ''
     let username = false
     let pass
-
+    let nodeAddress
+    let nodePort
+    let enableNextButton = false
+    let ready = false
+    let input
+    let showNode = false
+    let exitNode = "X"
+    let defualtPick = true
 
     const handleLogin = () => {
 
       let accountData = {
         walletName: walletName,
         password: password,
-        node: nodeInput.split(':')[0],
-        port: parseInt(nodeInput.split(':')[1])
+        node: nodeAddress,
+        port: nodePort
       }
         window.api.send('create-account', accountData);
 
@@ -36,34 +43,113 @@
         pass = true
         break;
       }
+        enableNextButton = false
+    }
 
+    $ : console.log('chosen node', nodeAddress + ':' + nodePort);
+
+    function chooseNode(node) {
+      console.log('thisnode', node);
+      if (input) {
+          nodeAddress = nodeInput.split(':')[0]
+          nodePort = parseInt(nodeInput.split(':')[1])
+      } else {
+          nodeAddress = node.url
+          nodePort = node.port
+      }
+      enableNextButton = true
+    }
+
+    $ : { if (nodePort == undefined) {
+          enableNextButton = false
+    } else {
+      nodeInput = nodeAddress + ':' + nodePort
+    } }
+
+    $ : console.log('nodeinput', nodeInput);
+
+
+    $: {
+        if (nodeInput.length > 0 || walletName.length > 0 || password.length > 0) {
+            //Enable add button
+            enableNextButton = true
+        } else {
+          enableNextButton = false
+        }
+    }
+
+    const showNodes = () => {
+      showNode =!showNode
+      if (!showNode) {
+        enableNextButton = true
+      }
+    }
+
+    function defualtPicker() {
+      console.log('picked');
+      if (defualtPick) {
+        nodeAddress = 'blocksum.org'
+        nodePort = 11898
+        enableNextButton = true
+      }
     }
 
 </script>
 
 <main in:fade>
+
+ <div id="create">
     <h1>Create Account</h1>
-    <div class="wrapper">
       {#if !username}
-      <label>Node</label>
-      <input type="text" placeholder="node:port" bind:value={nodeInput}>
-      <FillButton text="Next" on:click={()=> next('node')}/>
+      <div class="centerbox">
+
+        {#if !showNode}
+          <div class="pickNode">
+          <GreenButton text="Pick custom node" on:click={showNodes}/>
+          <GreenButton text="Defualt node" enabled={defualtPick} on:click={()=> defualtPicker()}/>
+          </div>
+        {/if}
+      </div>
+              {#if showNode}
+                <div class="nodeBox">
+                  <div on:click={showNodes}>{exitNode}</div>
+                  <h4>Node</h4>
+                    <div class="nodelist">
+                      {#each $nodelist as node}
+                          <div class="nodes" on:click={()=> chooseNode(node)}>
+                             <h4 class="nodes">{node.name}</h4>
+                          </div>
+                      {/each}
+                    </div>
+                  <br>
+                  <h4>Custom node</h4>
+                  <input placeholder="node:url" type="text" bind:value={nodeInput}>
+                  <FillButton text="Connect" on:click={()=> chooseNode(nodeInput, true)}/>
+                </div>
+              {/if}
+          <GreenButton  disabled={!enableNextButton} enabled={enableNextButton} text="Next" on:click={()=> next('node') }/>
       {/if}
+
+    <div class="wrapper">
+
       {#if username && !pass}
         <label>Username</label>
         <input type="text" placeholder="Satoshi" bind:value={walletName}>
-        <FillButton text="Next" on:click={()=> next('wallet')}/>
+        <GreenButton  disabled={!enableNextButton} enabled={enableNextButton} text="Next" on:click={()=> next('wallet')}/>
       {/if}
       {#if pass}
         <label>Password</label>
         <input type="password" placeholder="Something safe" bind:value={password}>
-        <FillButton text="Create" on:click={handleLogin}/>
+        <GreenButton  disabled={!enableNextButton} enabled={enableNextButton} text="Next" on:click={handleLogin}/>
       {/if}
         <!-- <FillButton text="back" url="/"/> -->
-    </div>
+      </div>
+  </div>
+
 </main>
 
-<style>
+<style lang="scss">
+
     main {
         height: 100vh;
         display: flex;
@@ -73,16 +159,34 @@
         z-index: 3;
     }
 
-    .wrapper {
+    #create {
+
+      padding: 3rem;
+      border-radius: 10px;
+      display: grid;
+      transition: .25s ease-in-out all;
+    }
+
+    .nodeBox {
+      display: block;
+      justify-content: center;
+      height: 500px;
+    }
+    .wrapper, .centerbox {
         display: flex;
         justify-content: center;
         align-items: flex-start;
         flex-direction: column;
     }
 
+    .pickNode {
+      display: inline-block;
+    }
+
     h1 {
-        margin: 0;
         color: white;
+        margin-bottom: 15% !important;
+        margin-top: 5% !important;
     }
 
     label {
@@ -90,10 +194,87 @@
         margin-top: 20px;
     }
 
-    input {
-        padding: 10px;
-        width: 200px;
-        border: none;
-        border-radius: 5px;
+    h2,h3,h4 {
+        color: white;
+        margin: 0;
+        font-family: "Montserrat"
     }
+
+    h3 {
+      font-size: 25px;
+      margin-bottom: 15px;
+      margin-left: -10px;
+    }
+
+    h4 {
+      font-size: 20px;
+    }
+
+
+    .nodelist {
+      height: 50%;
+      overflow-x: hidden;
+      overflow-y: scroll;
+    }
+
+    .nodes {
+      color: #f1f2f3;
+      font-family: 'Roboto Mono';
+      font-size: 17px;
+      padding: 7px;
+      cursor: pointer;
+    }
+
+    .nodes:hover {
+      background: rgba(0,0,0,0.1);
+      color: white;
+    }
+
+    input {
+      box-sizing: border-box;
+      background-color: var(--backgound-color);
+      border: 1px solid var(--card-border);
+      border-radius: 0.4rem;
+      color: var(--title-color);
+      padding: 0 10px;
+      margin-bottom: 20px;
+      position: fixed;
+      font-size: 17px !important;
+      width: 100%;
+      font-size: 16px;
+      height: 40px;
+      display: inline-flex;
+      position: relative;
+      font-family: 'Roboto Mono';
+      padding-left: 15px;
+      margin-top: 10px;
+      margin-left: -10px;
+      &:focus {
+        outline: none;
+        border: 1px solid var(--title-color);
+        }
+      }
+
+    .nodelist {
+    --scrollbarBG: transparent;
+    --thumbBG: #3337;
+    overflow: auto;
+
+    }
+     .nodelist::-webkit-scrollbar {
+      width: 8px;
+    }
+      .nodelist {
+      scrollbar-width: thin;
+      scrollbar-color: var(--thumbBG) var(--scrollbarBG);
+    }
+    .nodelist::-webkit-scrollbar-track {
+      background: var(--scrollbarBG);
+    }
+    .nodelist::-webkit-scrollbar-thumb {
+      background-color: var(--thumbBG) ;
+      border-radius: 3px;
+      border: 3px solid var(--scrollbarBG);
+    }
+
 </style>
