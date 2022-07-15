d6573f33dddbafc48a956a9f068018ba10f948f0
n9lsjr <rasmus@lingonbyran.se>
2022-07-16 01:53:26 +0200

    New CallerMenu component

---
+++ src/components/webrtc/CallerMenu.svelte
@@ -0,0 +1,138 @@
+<script>
+    //To handle true and false, or in this case show and hide.
+    import {fade, fly} from "svelte/transition";
+    import {cubicOut , cubicIn} from "svelte/easing"
+    import {get_avatar} from "$lib/utils/hugin-utils.js";
+    import {onDestroy, onMount} from "svelte";
+    import {user} from "$lib/stores/user.js";
+    import {createEventDispatcher} from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    export let paused = false
+    let avatar = get_avatar($user.call.sender)
+    let startTone = new Audio("/static/audio/startcall.mp3")
+    let endTone = new Audio("/static/audio/endcall.mp3")
+    let hangUp = false
+    let peer
+    let stream
+    // When incoming call and this get mounted we play the ringtone
+    onMount(() => {
+        startTone.play()
+    })
+
+    //When a user clicks answer
+    const endCall = () => {
+
+
+        dispatch('endCall')
+        //Variable to activate visual feedback
+        hangUp = true
+
+        //We delay the answerCall for routing purposes
+        window.api.endCall(peer, stream)
+
+        //We pause the ringtone and destroy the popup
+        endTone.play()
+
+    }
+
+    //As a precaution we pause the ringtone again when destroyed
+    onDestroy(() => {
+        endTone.pause()
+    })
+
+</script>
+
+
+<div in:fly="{{y: -100, duration:900, easing: cubicOut}}" out:fly="{{y: -100, duration: 900, easing: cubicIn}}" class="card" class:hangUp={hangUp} class:rgb={!hangUp}>
+    <audio bind:paused src="/static/audio/startcall.mp3"></audio>
+    <div class="inner-card">
+        <div class="caller">
+            <img class="avatar" src="data:image/png;base64,{avatar}" alt="">
+            <p>{$user.call.sender}</p>
+        </div>
+        <div class="options">
+            <!-- <a class="answer hover" on:click={handleAnswer} href="/webrtc">
+                <img src="/static/images/call.svg" alt="">
+            </a> -->
+            <div class="decline hover" on:click={endCall} >
+                <img src="/static/images/call-slash.svg" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<style lang="scss">
+    .card {
+        display: flex;
+        position: absolute;
+        padding: 1px;
+        top: 20px;
+        right: 105px;
+        height: 50px;
+        width: 300px;
+        background-color: #5f86f2;
+        border-radius: 5px;
+        box-shadow: 0 0 30px 10px rgba(0, 0, 0, 0.1);
+        border: 1px solid rgba(255,255,255, 0.1);
+        z-index: 200;
+    }
+
+    .inner-card {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        border-radius: 3px;
+        background-color: #202020;
+    }
+
+    .hangUp {
+      background-color: #5ff281;
+    }
+
+    .caller {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 10px;
+    }
+
+    .options {
+        display: flex;
+    }
+
+    .answer {
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+        padding: 15px;
+        transition: 250ms ease-in-out;
+        border-left: 1px solid rgba(255,255,255,0.1);
+    }
+
+    .decline {
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+        padding: 15px;
+        transition: 250ms ease-in-out;
+        border-left: 1px solid rgba(255,255,255,0.1);
+    }
+
+    .hover:hover {
+        background-color: rgba(255, 255, 255, 0.1);
+        cursor: pointer;
+    }
+
+    h3, p {
+        margin: 0;
+        color: rgba(255, 255, 255, 0.8);
+        font-weight: normal;
+        max-width: 120px;
+        overflow: hidden;
+        text-overflow: ellipsis
+    }
+</style>
