50490c25ea1799da478708192ff0337507e4cca8
n9lsjr <nilsjr@protonmail.com>
2022-08-01 19:49:42 +0200

    Added Loader component

---
+++ src/components/popups/Loader.svelte
@@ -0,0 +1,69 @@
+<script>
+    //To handle true and false, or in this case show and hide.
+    import {fade, fly} from "svelte/transition";
+    import {onMount} from "svelte";
+
+
+
+</script>
+
+<div in:fade="{{duration: 100}}" out:fade="{{duration: 170}}" class="backdrop" on:click|self>
+
+<div class="card">
+    <div>
+       <p in:fade="{{duration: 170}}">Loading Account</p>
+    </div>
+</div>
+
+</div>
+<style lang="scss">
+  .card {
+    box-sizing: border-box;
+    background-color: var(--card-background);
+    border: 1px solid var(--card-border);
+    padding: 20px;
+    border-radius: 0.4rem;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    @media only screen and (max-width: 1000px) {
+      max-width: 100%;
+    }
+
+    }
+
+    p {
+      margin: 0;
+      color: var(--title-color);
+    }
+
+    .backdrop {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(10, 10, 10, 0.8);
+        -webkit-backdrop-filter: blur(9px);
+        backdrop-filter: blur(9px);
+        margin-right: 85px;
+        z-index: 101;
+    }
+
+    .wrapper {
+        width: 100%;
+    }
+
+    h4 {
+        color: rgba(255, 255, 255, 0.8);
+        margin: 0;
+        margin-left: 4px;
+    }
+
+    .key {
+        background-color: rgba(255, 255, 255, 0.2);
+    }
+</style>
