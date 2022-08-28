040391bd7e7877815b5c982b026ecb069959cf99
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-28 15:22:32 +0200

    Update AddBoard.svelte

--- src/components/chat/AddBoard.svelte
+++ src/components/chat/AddBoard.svelte
@@ -1,6 +1,6 @@
 <script>
   //To handle true and false, or in this case show and hide.
-  import { fade } from "svelte/transition";
+  import { fade, fly } from "svelte/transition";
   import { createEventDispatcher, onMount } from "svelte";
   import GreenButton from "/src/components/buttons/GreenButton.svelte";
 
@@ -39,9 +39,9 @@
 
 </script>
 
-<div in:fade="{{duration: 100}}" out:fade="{{duration: 170}}" class="backdrop" on:click|self>
+<div in:fade="{{duration: 100}}" out:fade="{{duration: 100}}" class="backdrop" on:click|self>
 
-  <div class="card">
+  <div in:fly="{{y: 50}}" out:fly="{{y: -50}}" class="card">
       <h3>Add new board</h3>
       <input placeholder="Join or create a new public board" type="text" bind:value={text}>
       <GreenButton text={add_board} disabled={!enableAddBoardButton} enabled={enableAddBoardButton}
@@ -53,17 +53,16 @@
 <style lang="scss">
 
   .backdrop {
+    position: fixed;
     display: flex;
     justify-content: center;
     align-items: center;
-    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
     width: 100%;
-    height: 100%;
-    background-color: rgba(10, 10, 10, 0.8);
-    -webkit-backdrop-filter: blur(9px);
-    backdrop-filter: blur(9px);
-    margin-right: 85px;
-    z-index: 101;
+    background-color: var(--backdrop-color);
+    z-index: 103;
   }
 
   h3 {
@@ -73,7 +72,7 @@
 
 
   .card {
-    background-color: var(--card-background);
+    background-color: var(--backgound-color);
     border: 1px solid var(--card-border);
     padding: 20px;
     border-radius: 8px;
