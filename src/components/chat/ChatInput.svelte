dd1c521cce73ed5347e5802b60955088dd10753f
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-28 15:20:09 +0200

    Update ChatInput.svelte

--- src/components/chat/ChatInput.svelte
+++ src/components/chat/ChatInput.svelte
@@ -2,9 +2,11 @@
   import { createEventDispatcher } from "svelte";
   import sendIcon from "/static/images/send.png";
   import EmojiSelector from "svelte-emoji-selector";
-  import {webRTC} from "$lib/stores/user.js";
-  import {fade} from 'svelte/transition'
-  let off_chain
+  import { webRTC } from "$lib/stores/user.js";
+  import { fade } from "svelte/transition";
+  import { page } from "$app/stores";
+
+  let off_chain;
   const dispatch = createEventDispatcher();
 
   //This handles the emojis, lets fork the repo and make a darker theme.
@@ -32,7 +34,7 @@
   const sendMsg = () => {
     dispatch("message", {
       text: messageInput,
-      offChain: off_chain,
+      offChain: off_chain
     });
     //Reset input field
     messageInput = "";
@@ -44,33 +46,36 @@
   }
 
   $: {
-    off_chain = $webRTC.connected
-    console.log('offchain', off_chain)
+    off_chain = $webRTC.connected;
+    console.log("offchain", off_chain);
   }
 </script>
 
-<div class="wrapper" in:fade="{{duration: 400}}" out:fade="{{duration: 100}}">
+<div class="wrapper" class:border-bottom={$page.url.pathname === '/boards'}
+     class:border-top={$page.url.pathname === '/messages'} in:fade="{{duration: 400}}" out:fade="{{duration: 100}}">
   <input type="text" placeholder="Message.." bind:value={messageInput}>
   <EmojiSelector on:emoji={onEmoji} />
   <button disabled={!enableSend} class:enableSend={enableSend} on:click={sendMsg}><img src={sendIcon} height="15px"
-  alt=""></button>
+                                                                                       alt=""></button>
 </div>
 
 <style lang="scss">
 
   .wrapper {
     height: 77px;
-    box-sizing: border-box;
-    padding: 20px 20px 20px 20px;
+    padding: 1rem;
     width: 100%;
     display: flex;
     z-index: 3;
-    border-bottom-right-radius: 5px;
-    border-bottom-left-radius: 5px;
-    border-top: 1px solid var(--card-border);
-    border-bottom: 1px solid var(--card-border);
   }
 
+  .border-top {
+    border-top: 1px solid var(--border-color);
+  }
+
+  .border-bottom {
+    border-bottom: 1px solid var(--border-color);
+  }
 
   input {
     width: inherit;
@@ -80,10 +85,10 @@
     color: #ffffff;
     padding: 10px 15px;
     margin: 0;
-  }
 
-  input:focus {
-    outline: none;
+    &:focus {
+      outline: none;
+    }
   }
 
   button {
@@ -91,7 +96,6 @@
     justify-content: center;
     align-items: center;
     width: 40px;
-    box-sizing: border-box;
     background-color: rgba(255, 255, 255, 0.1);
     border: none;
     border-radius: 0 9px 9px 0;
@@ -99,21 +103,16 @@
     color: white;
     margin: 0;
     cursor: pointer;
-  }
 
-  button:focus {
-    outline: none;
+    &:focus {
+      outline: none;
+    }
   }
 
   .enableSend {
-    background-color: #3FD782;
-    border-color: #63e79f;
+    background-color: var(--success-color);
     color: #fff;
     font-weight: 600;
-
-    &:hover {
-      background-color: #63e79f;
-    }
   }
 
 </style>
