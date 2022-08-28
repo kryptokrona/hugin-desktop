4182b9d0f8f747a59fe9e947ed87be5b27475302
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-29 00:47:01 +0200

    colors and click event

--- src/components/buttons/Screenshare.svelte
+++ src/components/buttons/Screenshare.svelte
@@ -11,19 +11,19 @@
         <g transform="translate(2.0 2.0)">
           <path
             d="M15.5499992,0 C19.1099992,0 20,0.890000105 20,4.44000006 L20,10.7700005 C20,14.3300004 19.1099994,15.2100821 15.5599995,15.2100821 L4.44000006,15.2100821 C0.890000105,15.2199991 0,14.3299997 0,10.7799997 L0,4.44000006 C0,0.890000105 0.890000105,0 4.44000006,0 L15.5499992,0 Z"
-            stroke="#292D32" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round"
+            stroke="var(--text-color)" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round"
             stroke-dasharray=" 0.0 0.0"></path>
         </g>
         <g transform="translate(12.0 17.219970703125)">
-          <path d="M0,0 L0,4.78000069" stroke="#292D32" stroke-width="1.5" fill="none" stroke-linecap="round"
+          <path d="M0,0 L0,4.78000069" stroke="var(--text-color)" stroke-width="1.5" fill="none" stroke-linecap="round"
                 stroke-linejoin="round" stroke-dasharray=" 0.0 0.0"></path>
         </g>
         <g transform="translate(2.0 13.0)">
-          <path d="M0,0 L20,0" stroke="#292D32" stroke-width="1.5" fill="none" stroke-linecap="round"
+          <path d="M0,0 L20,0" stroke="var(--text-color)" stroke-width="1.5" fill="none" stroke-linecap="round"
                 stroke-linejoin="round" stroke-dasharray=" 0.0 0.0"></path>
         </g>
         <g transform="translate(7.5 22.0)">
-          <path d="M0,0 L9,0" stroke="#292D32" stroke-width="1.5" fill="none" stroke-linecap="round"
+          <path d="M0,0 L9,0" stroke="var(--text-color)" stroke-width="1.5" fill="none" stroke-linecap="round"
                 stroke-linejoin="round" stroke-dasharray=" 0.0 0.0"></path>
         </g>
       </g>
