aafdeefced457b4789723b921c948853f3255948
n9lsjr <nilsjr@protonmail.com>
2022-08-31 01:31:36 +0200

    Added transactions component and buttons

---
+++ src/components/finance/Transactions.svelte
@@ -0,0 +1,135 @@
+<script>
+import { user } from "$lib/stores/user";
+import {fade} from "svelte/transition";
+import {onMount} from "svelte";
+import Forward from "/src/components/buttons/Forward.svelte";
+import Backward from "/src/components/buttons/Backward.svelte";
+
+let pageNum = 0
+let pages
+let txList = []
+onMount(() => {
+    getTransactions(pageNum)
+})
+
+async function getTransactions() {
+    console.log('pagenumber', pageNum)
+
+    let startIndex = pageNum * 10
+
+    if (pageNum === 0) {
+        startIndex = 0
+    }
+
+    let transactions = await window.api.getTransactions(startIndex);
+    $user.transactions = transactions.pageTx
+    pages = transactions.pages
+    txList = $user.transactions
+    console.log(transactions)
+
+
+}
+$: console.log('pageNum', pageNum)
+$: pageNum
+$: txList
+
+
+</script>
+
+<div in:fade class="wrapper">
+<div class="header">
+    <h3>Transactions</h3>
+    <div>
+        {#if (pageNum > 0)}
+            <Backward on:click={() => getTransactions(pageNum--)}/>
+        {/if}
+        {#if (pages >= pageNum)}
+            <Forward on:click={() => getTransactions(pageNum++)}/>
+        {/if}
+    </div>
+</div>
+{#if $user.transactions.length > 0}
+    <div class="transactions">
+        {#each txList as tx}
+            <div class="tx">
+                <p>{tx.hash.substring(0, 8) + "..." + tx.hash.substring(56, tx.hash.length)}</p>
+                <p>{tx.amount}</p>
+            </div>
+        {/each}
+    </div>
+{:else}
+    <div class="notx">
+        <h3>No transactions</h3>
+    </div>
+{/if}
+</div>
+
+<style lang="scss">
+.wrapper {
+height: 75.5%;
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid var(--border-color);
+  height: 50px;
+  padding: 0 30px;
+
+  p {
+    margin: 0;
+  }
+}
+
+.transactions {
+    height: 100%;
+    width: 100%;
+    overflow: scroll;
+    box-sizing: border-box;
+    --scrollbarBG: transparent;
+    --thumbBG: #3337;
+    overflow: auto;
+    scrollbar-width: thin;
+     scrollbar-color: var(--thumbBG) var(--scrollbarBG);
+     height: 300px;
+}
+
+ .transactions::-webkit-scrollbar {
+  width: 8px;
+}
+.transactions::-webkit-scrollbar-track {
+  background: var(--scrollbarBG);
+}
+.transactions::-webkit-scrollbar-thumb {
+  background-color: var(--thumbBG) ;
+  border-radius: 3px;
+  border: 3px solid var(--scrollbarBG);
+}
+
+.tx {
+  display: flex;
+  box-sizing: border-box;
+  justify-content: space-between;
+  padding: 0 30px;
+  width: 100%;
+
+  &:hover {
+    background-color: var(--border-color);
+    cursor: pointer;
+  }
+
+  &:active {
+    background-color: var(--primary-color);
+    color: #121212;
+  }
+}
+}
+
+.notx {
+display: flex;
+height: 80vh;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+}
+</style>
