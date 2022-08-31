d0083353de31d800c2fc2536103b4e6b255f5201
n9lsjr <nilsjr@protonmail.com>
2022-08-31 20:28:51 +0200

    Added svg message icon

---
+++ src/components/buttons/MessageIcon.svelte
@@ -0,0 +1,26 @@
+<svg on:click width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
+    <g id="vuesaxlinearsms">
+      <g id="vuesaxlinearsms">
+        <g id="sms">
+          <path d="M7 20.5C4 20.5 2 19 2 15.5L2 8.5C2 5 4 3.5 7 3.5L17 3.5C20 3.5 22 5 22 8.5L22 15.5C22 19 20 20.5 17 20.5L7 20.5Z" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M17 9L13.87 11.5C12.84 12.32 11.15 12.32 10.12 11.5L7 9" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <g id="Vector-2">
+            <path d="M24 0L24 24L0 24L0 0L24 0Z" id="Vector" fill="none" fill-rule="evenodd" stroke="none" />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+
+  <style lang="scss">
+
+    svg {
+        transition: 200ms ease-in-out;
+        cursor: pointer;
+        margin: 10px;
+    
+        &:hover {
+          opacity: 80%;
+        }
+      }
+    </style>
