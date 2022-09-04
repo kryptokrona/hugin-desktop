d5ee207193b7f27f5ccfc9f2a47ccc20c0358c76
Lukas <36674091+Swepool@users.noreply.github.com>
2022-09-04 23:57:05 +0200

    Create HomeIcon.svelte

---
+++ src/components/buttons/HomeIcon.svelte
@@ -0,0 +1,30 @@
+<svg version="1.1" width="22px" height="22px" viewBox="0 0 22.0 22.0" xmlns="http://www.w3.org/2000/svg"
+     xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <clipPath id="95118481111044510611111752">
+      <path d="M880,0 L880,886 L0,886 L0,0 L880,0 Z"></path>
+    </clipPath>
+  </defs>
+  <g transform="translate(-621.0 -189.0)">
+    <g clip-path="url(#95118481111044510611111752)">
+      <g transform="translate(620.0 188.0)">
+        <g transform="translate(2.0 2.0174560546875)">
+          <path
+            d="M1.63000011,5.02258444 C0.730000138,5.72258443 0,7.21258366 0,8.34258366 L0,15.7525845 C0,18.0725844 1.8900001,19.9725838 4.21000004,19.9725838 L15.7900009,19.9725838 C18.1100008,19.9725838 20,18.0725846 20,15.7625847 L20,8.482584 C20,7.27258396 19.1900008,5.72258466 18.2000008,5.03258467 L12.0200005,0.702584445 C10.6200005,-0.277415574 8.37000048,-0.227415621 7.02000046,0.822584331 L1.63000011,5.02258444 Z"
+            stroke="var(--text-color)" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round"
+            stroke-dasharray=" 0.0 0.0"></path>
+        </g>
+        <g transform="translate(12.0 14.989990234375)">
+          <path d="M0,3 L0,0" stroke="var(--text-color)" stroke-width="1.5" fill="none" stroke-linecap="round"
+                stroke-linejoin="round" stroke-dasharray=" 0.0 0.0"></path>
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
+
+<style>
+  svg {
+      margin-top: 4px;
+  }
+</style>
