3c278fae1c53cebf601417575227ead97d23d1c7
n9lsjr <nilsjr@protonmail.com>
2022-09-07 23:21:20 +0200

    Addgroup functions, error handling etc

--- src/components/chat/AddGroup.svelte
+++ src/components/chat/AddGroup.svelte
@@ -3,30 +3,52 @@
   import { fade, fly } from "svelte/transition";
   import { createEventDispatcher, onMount } from "svelte";
   import GreenButton from "/src/components/buttons/GreenButton.svelte";
-  import { groups } from "$lib/stores/user";
-
+  import Button from "/src/components/buttons/Button.svelte";
+  import { groups, notify } from "$lib/stores/user";
+  import {get_avatar} from "$lib/utils/hugin-utils.js";
   const dispatch = createEventDispatcher();
 
-  let enableAddBoardButton = false;
-  let text = "";
-  let board;
-  let add_board = "Add";
+  let enableAddGroupButton = false;
+  let create_group = "Create";
   let name = ""
+  let key = ""
   $: {
-    if (text.length > 0 && name.length > 0) {
+    if (key.length === 64 && name.length > 0) {
       //Enable add button
-      enableAddBoardButton = true;
+      enableAddGroupButton = true;
 
     } else {
-      enableAddBoardButton = false;
+      enableAddGroupButton = false;
     }
   }
 
   // Dispatch the inputted data
-  const addGroup = () => {
+  const addGroup = (g) => {
+    if ($groups.groupArray.some(g => g.name === name)) {
+      $notify.errors.push({
+      m: "Group name already exists",
+      n: "Error",
+      h: parseInt(Date.now())
+    })
+
+    $notify.errors = $notify.errors
+    console.log($notify.errors);
+    return
+  }
+
+    if ($groups.groupArray.some(g => g.key === key)) {
+      $notify.errors.push({
+      m: "This group key already exists",
+      n: "Error",
+      h: parseInt(Date.now())
+    })
+
+    $notify.errors = $notify.errors
+    return;
+  }
     // Dispatch the inputted data
     dispatch("addGroup", {
-      key: text,
+      key: key,
       name: name
     });
     enableAddBoardButton = false;
@@ -34,26 +56,35 @@
   };
 
   window.addEventListener("keyup", e => {
-    if (enableAddBoardButton && e.keyCode === 13) {
-      addBoard(text);
+    if (enableAddGroupButton && e.keyCode === 13) {
+      addGroup();
     }
   });
 
+  const createGroup = async () => {
+
+   key = await window.api.createGroup()
+  }
+
+  $: key
 
 </script>
 
 <div in:fade="{{duration: 100}}" out:fade="{{duration: 100}}" class="backdrop" on:click|self>
-
   <div in:fly="{{y: 50}}" out:fly="{{y: -50}}" class="card">
-
-      <h3>Name your group</h3>
-      <input placeholder="Name your group" type="text" bind:value={name}>
-      <h3>Add new private group</h3>
-      <input placeholder="Input group key" type="text" bind:value={text}>
-      <GreenButton text={add_board} disabled={!enableAddBoardButton} enabled={enableAddBoardButton}
+    
+    <h3 in:fade>Name your group</h3>
+    <input placeholder="Name your group" type="text" bind:value={name}>
+      <Button disabled={false} text="Generate key" on:click={() => createGroup()}/>
+    <div class="key-wrapper" in:fade>
+    <input placeholder="Input group key" type="text" bind:value={key}>
+    {#if key.length}
+    <img in:fade class="avatar" src="data:image/png;base64,{get_avatar(key)}" alt="">
+    {/if}
+    </div>
+      <GreenButton text={create_group} disabled={!enableAddGroupButton} enabled={enableAddGroupButton}
                    on:click={()=> addGroup()} />
   </div>
-
 </div>
 
 <style lang="scss">
@@ -82,8 +113,8 @@
     border: 1px solid var(--card-border);
     padding: 20px;
     border-radius: 8px;
-    width: 300px;
-    height: 180px;
+    width: 250px;
+    height: 300px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -105,4 +136,27 @@
       border: 1px solid rgba(255, 255, 255, 0.6);
     }
   }
+
+  .key {
+    font-family: "Roboto Mono";
+    font-size: 17px;
+  }
+  
+  .avatar {
+    width: 40px;
+    height: 40px;
+  }
+
+  .key-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  p {
+    font-size: 12px;
+    color: white;
+    font-family: "Montserrat";
+  }
+
 </style>
