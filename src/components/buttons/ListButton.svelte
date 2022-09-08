c7751e38406a244277616e39eeb7a5c603b6736f
n9lsjr <nilsjr@protonmail.com>
2022-09-08 22:26:09 +0200

    Added listbutton

---
+++ src/components/buttons/ListButton.svelte
@@ -0,0 +1,27 @@
+<svg on:click width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
+    <g id="vuesaxlinearfirstline">
+      <g id="vuesaxlinearfirstline">
+        <g id="firstline">
+          <path d="M14 4.5L21 4.5" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M14 9.5L21 9.5" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M3 14.5L21 14.5" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M3 19.5L21 19.5" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M9.5 5.57C9.5 4.45 9.05 4 7.92 4L5.07 4C3.95 4 3.5 4.45 3.5 5.57L3.5 8.42C3.5 9.55 3.95 10 5.07 10L7.92 10C9.05 10 9.5 9.55 9.5 8.43L9.5 5.57Z" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M1.90735e-06 24L0 0L24 -1.90735e-06L24 24L1.90735e-06 24Z" id="Vector" fill="none" fill-rule="evenodd" stroke="none" />
+        </g>
+      </g>
+    </g>
+  </svg>
+
+  
+<style lang="scss">
+    svg {
+      transition: 200ms ease-in-out;
+      cursor: pointer;
+  
+      &:hover {
+        opacity: 80%;
+      }
+    }
+  
+  </style>
