80409b0d6b521667f6066a406e29b805dd911ad2
n9lsjr <nilsjr@protonmail.com>
2022-09-08 21:48:24 +0200

    Added group component

---
+++ src/components/chat/Group.svelte
@@ -0,0 +1,102 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import { fade } from "svelte/transition";
+  import { get_avatar } from "$lib/utils/hugin-utils.js";
+  import { user, groups } from "$lib/stores/user.js";
+
+  export let group;
+  const dispatch = createEventDispatcher();
+
+  const printThis = (contact) => {
+    dispatch("print")
+  };
+
+
+</script>
+
+<div class="card"
+     in:fade="{{duration: 100}}" out:fade="{{duration: 100}}"
+     class:active={$groups.thisGroup.key === group.key}
+     on:click={(e) => printThis(group)}>
+
+  {#if group.new}
+    <div class:unread={group.new}></div>
+  {/if}
+
+  <img class="avatar"
+       src="data:image/png;base64,{get_avatar(group.key)}" alt="">
+  <div class="content">
+    <h4>{group.name}</h4>
+    <p>{group.msg}</p>
+  </div>
+
+</div>
+
+<style lang="scss">
+
+  .card {
+    display: flex;
+    height: 80px;
+    padding: 1rem;
+    width: 100%;
+    color: var(--title-color);
+    border-bottom: 1px solid var(--border-color);
+    transition: 200ms ease-in-out;
+    cursor: pointer;
+    opacity: 0.9;
+
+    &:hover {
+      color: white;
+      opacity: 1.0;
+      background-color: var(--card-border);
+    }
+  }
+
+  .avatar {
+    margin-bottom: 10px;
+    opacity: 0.92;
+    cursor: pointer;
+  }
+
+  .content {
+    margin-left: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  h4 {
+    margin: 0;
+    white-space: nowrap;
+    max-width: 180px;
+    overflow: hidden;
+    font-family: "Montserrat", sans-serif;
+    text-overflow: ellipsis;
+    font-weight: bold;
+  }
+
+  p {
+    margin: 5px 0 0 0;
+    white-space: nowrap;
+    max-width: 200px;
+    overflow: hidden;
+    font-size: 12px;
+    text-overflow: ellipsis;
+    font-family: "Montserrat", sans-serif;
+  }
+
+  .unread {
+    animation: border_rgb 30s infinite;
+    background-color: white;
+    width: 5px;
+    height: 2px;
+    border-radius: 30%;
+    left: 340px;
+    margin-top: 25px;
+    position: absolute;
+  }
+
+  .active {
+    animation: border_rgb 10s infinite;
+  }
+</style>
