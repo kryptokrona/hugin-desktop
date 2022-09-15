acdf7a5059a06ab3b5c3690c7c3cdf8a11960ed8
n9lsjr <nilsjr@protonmail.com>
2022-09-15 16:58:54 +0200

    Colors

--- src/components/buttons/VideoSlash.svelte
+++ src/components/buttons/VideoSlash.svelte
@@ -1,11 +1,14 @@
+<script>
+  let color = "red"
+</script>
 <svg on:click width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
     <g id="vuesaxlinearvideo-slash">
       <g id="vuesaxlinearvideo-slash">
         <g id="video-slash">
-          <path d="M16.63 7.58008C16.63 7.58008 16.66 6.63008 16.63 6.32008C16.46 4.28008 15.13 3.58008 12.52 3.58008L6.21 3.58008C3.05 3.58008 2 4.63008 2 7.79008L2 16.2101C2 17.4701 2.38 18.7401 3.37 19.5501L4 20.0001" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-          <path d="M16.74 10.95L16.74 16.21C16.74 19.3699 15.69 20.42 12.53 20.42L7.26001 20.42" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-          <path d="M22 6.73999L22 15.81C22 17.48 20.88 18.06 19.52 17.1L16.74 15.15" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-          <path d="M22.02 2.18994L2.02002 22.1899" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M16.63 7.58008C16.63 7.58008 16.66 6.63008 16.63 6.32008C16.46 4.28008 15.13 3.58008 12.52 3.58008L6.21 3.58008C3.05 3.58008 2 4.63008 2 7.79008L2 16.2101C2 17.4701 2.38 18.7401 3.37 19.5501L4 20.0001" id="Vector" fill="none" fill-rule="evenodd" stroke="{color}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M16.74 10.95L16.74 16.21C16.74 19.3699 15.69 20.42 12.53 20.42L7.26001 20.42" id="Vector" fill="none" fill-rule="evenodd" stroke="{color}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M22 6.73999L22 15.81C22 17.48 20.88 18.06 19.52 17.1L16.74 15.15" id="Vector" fill="none" fill-rule="evenodd" stroke="{color}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M22.02 2.18994L2.02002 22.1899" id="Vector" fill="none" fill-rule="evenodd" stroke="{color}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
           <path d="M24 0L24 24L0 24L0 0L24 0Z" id="Vector" fill="none" fill-rule="evenodd" stroke="none" />
         </g>
       </g>
