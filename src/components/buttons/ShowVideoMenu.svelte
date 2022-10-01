cf344f9d72f5d56672244300e86e3130e2e9c744
n9lsjr <nilsjr@protonmail.com>
2022-10-01 11:34:39 +0200

    New button

---
+++ src/components/buttons/ShowVideoMenu.svelte
@@ -0,0 +1,24 @@
+<svg on:click width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
+    <g id="vuesaxlinearmaximize">
+      <g id="vuesaxlinearmaximize">
+        <g id="maximize">
+          <path d="M2 9.98L2 9C2 4 4 2 9 2L15 2C20 2 22 4 22 9L22 15C22 20 20 22 15 22L14 22" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M13 11L18.01 5.97998L14 5.97998" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M18.01 5.97998L18.01 9.98998" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M11 18.85C11 21.1 10.1 22 7.85 22L5.15 22C2.9 22 2 21.1 2 18.85L2 16.15C2 13.9 2.9 13 5.15 13L7.85 13C10.1 13 11 13.9 11 16.15L11 18.85Z" id="Vector" fill="none" fill-rule="evenodd" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M24 0L24 24L0 24L0 0L24 0Z" id="Vector" fill="none" fill-rule="evenodd" stroke="none" />
+        </g>
+      </g>
+    </g>
+  </svg>
+
+  <style lang="scss">
+    svg {
+      transition: 200ms ease-in-out;
+      cursor: pointer;
+  
+      &:hover {
+        opacity: 80%;
+      }
+    }
+  </style>
