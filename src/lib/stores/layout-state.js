ebd7e8772244748070dda979e6566184831bbb88
Lukas <36674091+Swepool@users.noreply.github.com>
2022-10-03 23:29:10 +0200

    state

--- src/lib/stores/layout-state.js
+++ src/lib/stores/layout-state.js
@@ -3,5 +3,9 @@ import { writable } from "svelte/store";
 //Default values at start
 export const layoutState = writable({
   hideChatList: true,
-  hideGroupList: false
+  hideGroupList: false,
+})
+
+export const videoGrid = writable({
+  showChat: false
 })
