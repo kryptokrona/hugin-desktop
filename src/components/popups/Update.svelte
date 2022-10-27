1a7d17e7443e91a71e299b557e108966315b99ae
Lukas <36674091+Swepool@users.noreply.github.com>
2022-10-27 22:07:07 +0200

    stylleee

--- src/components/popups/Update.svelte
+++ src/components/popups/Update.svelte
@@ -1,122 +1,79 @@
 <script>
-    import { onMount, onDestroy } from 'svelte';   
-    import { notify } from "$lib/stores/user.js";
-    import {fade, fly} from "svelte/transition";
-    import {cubicOut , cubicIn} from "svelte/easing"
-    import { createEventDispatcher } from 'svelte';
-    import {get_avatar} from "$lib/utils/hugin-utils.js";
+  import { onMount, onDestroy } from "svelte";
+  import { notify } from "$lib/stores/user.js";
+  import { fade, fly } from "svelte/transition";
+  import { cubicOut, cubicIn } from "svelte/easing";
+  import { createEventDispatcher } from "svelte";
+  import { get_avatar } from "$lib/utils/hugin-utils.js";
+  import GreenButton from "$components/buttons/GreenButton.svelte";
 
-	const dispatch = createEventDispatcher();
-    let timer
-    export let updates
-    export let platform
+  const dispatch = createEventDispatcher();
+  let timer;
+  export let updates;
+  export let platform;
 
-    onMount(()=> 
-    
-	timer = setTimeout(function() {
-        hideNotification(updates.hash)
-   }, 20 * 1000)
-)
-    onDestroy(()=> {
-        clearTimeout(timer)
-        hideNotification()
-    })
+  onMount(() =>
 
-	function hideNotification(id) {
-		dispatch('hide', {
-			hash: updates.hash
-		});
-	}
+    timer = setTimeout(function() {
+      hideNotification(updates.hash);
+    }, 20 * 1000)
+  );
+  onDestroy(() => {
+    clearTimeout(timer);
+    hideNotification();
+  });
 
-    function update() {
-        window.api.send('update', platform)
-        console.log('update!')
-    }
+  function hideNotification(id) {
+    dispatch("hide", {
+      hash: updates.hash
+    });
+  }
 
-  </script>
+  function update() {
+    window.api.send("update", platform);
+    console.log("update!");
+  }
+
+</script>
 
 <div in:fly="{{x: 200, duration:200, easing: cubicOut}}" out:fly="{{y: -200, duration: 200, easing: cubicIn}}" class="card">
-    <div class="inner-card">
-        <div class="header">
-            <img class="avatar" src="data:image/png;base64,{get_avatar(updates.key)}" alt="">
-            <h4 >{updates.name}</h4>
-        </div>
-            <div on:click={update}><p>Update</p></div>
-            <div on:click={hideNotification}><p>Remind Later</p></div>
-         <br>
+    <div class="header">
+      <img src="/icon.png" width="20px" height="20px" alt="">
+      <h3>Update available!</h3>
+    </div>
+    <div class="buttons">
+      <GreenButton text="Update" on:click={update} />
+      <h4 on:click={hideNotification}>Later</h4>
     </div>
 </div>
 
-  <style>
-      .card {
+<style lang="scss">
+    .card {
         display: flex;
-        padding: 1px;
-        height: 68px;
-        width: 300px;
+        padding: 1rem;
+        gap: 1rem;
         flex-direction: column;
-        box-sizing: border-box;
+        justify-content: space-between;
         border-radius: 5px;
         box-shadow: 0 0 30px 10px rgba(0, 0, 0, 0.5);
-        border: 1px solid rgba(255,255,255, 0.1);
+        border: 1px solid rgba(255, 255, 255, 0.1);
         z-index: 99999;
-    }
-
-    .inner-card {
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-        border-radius: 3px;
         background-color: #111111;
     }
 
-    .avatar {
-        height: 30px;
-        width: 30px;
-    }
-
-    .name {
-        font-size: 12px;
+    .buttons {
         display: flex;
-        font-weight: bold;
-        padding-right: 10px;
-    }
+      align-items: center;
+      gap: 1rem;
 
-    
-    p {
-        margin-top: 0px;
-    font-size: 12px;
-    display: contents;
-    }
-
-    h4 {
+        p {
         margin: 0;
-        color: rgba(255, 255, 255, 0.8);
-        font-weight: normal;
-        max-width: 120px;
-        overflow: hidden;
-        text-overflow: ellipsis
+        }
     }
 
     .header {
-        margin-left: 10px;
-        padding-top: 5px;
-        align-items: center;
         display: flex;
-        align-items: center;
-        gap: 0.25rem;
+        gap: 0.5rem;
     }
 
-    .message {
-        font-family: "Montserrat";
-        font-size: 12px;
-        display: inline-flex;
-        margin-left: 44px;
-    }
-
-    .success {
-        color: var(--success-color)
-    }
-
-
-    
-  </style>
+</style>
