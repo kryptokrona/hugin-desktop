8a4a1e453037445f5fea4ac9cc85b0874aaabf39
Lukas <36674091+Swepool@users.noreply.github.com>
2022-10-30 17:44:55 +0100

    Emoji picker chatinput

---
+++ src/components/icons/Emoji.svelte
@@ -0,0 +1,21 @@
+<svg width="20px" height="20px" viewBox="0 0 22 22" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <g id="Linear" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Essetional" transform="translate(-429.000000, -381.000000)">
+            <g id="emoji-happy" transform="translate(428.000000, 380.000000)">
+                <path d="M15,22 C20,22 22,20 22,15 L22,9 C22,4 20,2 15,2 L9,2 C4,2 2,4 2,9 L2,15 C2,20 4,22 9,22 L15,22 Z"
+                      id="Vector" opacity="50%" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                      stroke-dasharray="0,0" fill-rule="nonzero"></path>
+                <path d="M15.5,9.75 C14.6715729,9.75 14,9.07842714 14,8.25 C14,7.42157286 14.6715729,6.75 15.5,6.75 C16.3284271,6.75 17,7.42157286 17,8.25 C17,9.07842714 16.3284271,9.75 15.5,9.75 Z"
+                      id="Vector" opacity="50%" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                      stroke-dasharray="0,0"></path>
+                <path d="M8.5,9.75 C7.67157286,9.75 7,9.07842714 7,8.25 C7,7.42157286 7.67157286,6.75 8.5,6.75 C9.32842714,6.75 10,7.42157286 10,8.25 C10,9.07842714 9.32842714,9.75 8.5,9.75 Z"
+                      id="Vector" opacity="50%" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                      stroke-dasharray="0,0"></path>
+                <path d="M15.6000004,13.3000488 C16.1000004,13.3000488 16.5,13.7000484 16.5,14.2000484 C16.5,16.6900485 14.49,18.7000494 12,18.7000494 C9.50999999,18.7000494 7.5,16.6900485 7.5,14.2000484 C7.5,13.7000484 7.89999962,13.3000488 8.39999962,13.3000488 L15.6000004,13.3000488 Z"
+                      id="Vector" opacity="50%" stroke="#f5f5f5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                      stroke-dasharray="0,0" fill-rule="nonzero"></path>
+                <path d="M24,0 L24,24 L0,24 L0,0 L24,0 Z" id="Vector" opacity="0"></path>
+            </g>
+        </g>
+    </g>
+</svg>
