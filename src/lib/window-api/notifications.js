e23f9d43add307ff6640bf07e40f2af8d89ba4b6
Lukas <36674091+Swepool@users.noreply.github.com>
2022-11-01 03:24:19 +0100

    start to separate notifications into own file

---
+++ src/lib/window-api/notifications.js
@@ -0,0 +1,29 @@
+import {browser} from "$app/environment";
+import toast from "svelte-french-toast";
+
+
+if (browser) {
+
+    window.api.receive('error_msg', async (error) => {
+        console.log('Error', error)
+        toast.error(`${error.message}`, {
+            position: 'top-right',
+            style: 'border-radius: 5px; background: #171717; border: 1px solid #252525; color: #fff;',
+        })
+    })
+
+    window.api.receive('node-not-ok', () => {
+        toast.error('Could not connect to node', {
+            position: 'top-right',
+            style: 'border-radius: 5px; background: #171717; border: 1px solid #252525; color: #fff;',
+        })
+    })
+
+    window.api.receive('sent_tx', async (data) => {
+        toast.success(`${data.message}`, {
+            position: 'top-right',
+            style: 'border-radius: 5px; background: #171717; border: 1px solid #252525; color: #fff;',
+        })
+    })
+
+}
