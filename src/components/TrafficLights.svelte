7123588b69873003fac02068d9e59e99ab250035
Lukas <36674091+Swepool@users.noreply.github.com>
2022-11-04 17:26:14 +0100

    Update TrafficLights.svelte

--- src/components/TrafficLights.svelte
+++ src/components/TrafficLights.svelte
@@ -1,21 +1,33 @@
-<div class="traffic draggable">
-    <div on:click="{() => window.api.send('close')}" class="light"></div>
-    <div on:click="{() => window.api.send('min')}" class="light"></div>
+<div class="top-bar">
+    <div class="traffic">
+        <div on:click="{() => window.api.send('close')}" class="light"></div>
+        <div on:click="{() => window.api.send('min')}" class="light"></div>
+    </div>
+    <div class="top-drag draggable"></div>
 </div>
 
 <style lang="scss">
-.traffic {
+
+  .top-bar {
     position: fixed;
     top: 0;
     display: flex;
     width: 100%;
+    z-index: 9999;
+  }
+
+  .traffic {
+    display: flex;
     gap: 5px;
     padding: 10px;
-    z-index: 9999;
-    height: 25px;
-}
+  }
 
-.light {
+  .top-drag {
+    height: 25px;
+    width: 100%;
+  }
+
+  .light {
     width: 13px;
     height: 13px;
     border-radius: 20px;
@@ -24,15 +36,15 @@
     cursor: pointer;
 
     &:first-child {
-        &:hover {
-            background-color: var(--warn-color);
-        }
+      &:hover {
+        background-color: var(--warn-color);
+      }
     }
 
     &:last-child {
-        &:hover {
-            background-color: var(--alert-color);
-        }
+      &:hover {
+        background-color: var(--alert-color);
+      }
     }
-}
+  }
 </style>
