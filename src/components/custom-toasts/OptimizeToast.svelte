e3f40915539936c6cf39feaa2c15a410b906f6a9
Lukas <36674091+Swepool@users.noreply.github.com>
2022-11-06 02:11:12 +0100

    Create OptimizeToast.svelte

---
+++ src/components/custom-toasts/OptimizeToast.svelte
@@ -0,0 +1,15 @@
+<script>
+    import toast_ from 'svelte-french-toast';
+
+    export let toast;
+</script>
+
+<span>
+    Failed to send, want to optimize?
+    <button on:click={() => window.api.send('optimize')}>Optimize</button>
+    <button on:click={() => toast_.dismiss(toast.id)}>Dismiss</button>
+</span>
+
+<style>
+
+</style>
