5bf5226c30f3d3ba7606ecde8a8c30e9544388a6
Lukas <Lukas.nyberg@gmail.com>
2022-12-01 22:29:04 +0100

    fix bug

--- src/lib/components/chat/ChatInput.svelte
+++ src/lib/components/chat/ChatInput.svelte
@@ -1,113 +1,118 @@
 <script>
-    import {createEventDispatcher, onDestroy, onMount} from 'svelte'
-    import SendIcon from '$lib/components/icons/SendIcon.svelte'
-    import {boards, webRTC, groups, beam} from '$lib/stores/user.js'
-    import {page} from '$app/stores'
-    import {user} from '$lib/stores/user.js'
-    import Emoji from "$lib/components/icons/Emoji.svelte";
-    import {layoutState, videoGrid} from '$lib/stores/layout-state.js'
-    import 'emoji-picker-element';
-    import {sleep} from "$lib/utils/utils.js";
+  import {createEventDispatcher, onDestroy, onMount} from 'svelte'
+  import SendIcon from '$lib/components/icons/SendIcon.svelte'
+  import {boards, webRTC, groups, beam} from '$lib/stores/user.js'
+  import {page} from '$app/stores'
+  import {user} from '$lib/stores/user.js'
+  import Emoji from "$lib/components/icons/Emoji.svelte";
+  import {layoutState, videoGrid} from '$lib/stores/layout-state.js'
+  import 'emoji-picker-element';
+  import {sleep} from "$lib/utils/utils.js";
 
-    const dispatch = createEventDispatcher()
-    export let rtc = false
+  const dispatch = createEventDispatcher()
+  export let rtc = false
 
-    let openEmoji;
-    let emojiPicker
-    let messageField
-    let off_chain
-    let mount = false
-    let activeBeam = false
+  let openEmoji;
+  let emojiPicker
+  let messageField
+  let off_chain
+  let mount = false
+  let activeBeam = false
 
-    onMount(async () => {
-        mount = true
-        await sleep(1000)
-        emojiPicker = document.querySelector('emoji-picker')
-        emojiPicker.addEventListener('emoji-click', (e) => onEmoji(e.detail.unicode))
-    })
+  onMount(async () => {
+    mount = true
+    await sleep(1000)
+    emojiPicker = document.querySelector('emoji-picker')
+    emojiPicker.addEventListener('emoji-click', (e) => onEmoji(e.detail.unicode))
+  })
 
-    onDestroy(() => {
-        emojiPicker = ''
-        window.api.removeAllListeners("emoji-click");
-    })
+  onDestroy(() => {
+    emojiPicker = ''
+    window.api.removeAllListeners("emoji-click");
+  })
 
-    let shiftKey
-    const keyup = (e) => {
-      if(e.key === 'Shift') shiftKey = false
-        if (messageInput && !shiftKey && e.key === 'Enter') {
-             sendMsg()
-         }
+  let shiftKey
+  const keyup = (e) => {
+    if (e.key === 'Shift') shiftKey = false
+    if (messageInput && !shiftKey && e.key === 'Enter') {
+      sendMsg()
+    }
+    if(shiftKey && e.key ==='Enter') {
       autosize()
     }
+  }
 
-    const keydown = (e) => {
-      if(e.key === 'Shift') shiftKey = true
+  const keydown = (e) => {
+    if (e.key === 'Shift') shiftKey = true
+  }
+
+  //This handles the emojis, lets fork the repo and make a darker theme.
+  function onEmoji(emoji) {
+    if (messageInput) {
+      messageInput += emoji
+    } else messageInput = emoji
+    openEmoji = false
+  }
+
+  //Input data to dispatch
+  let messageInput
+
+  //To handle button disabled enabled
+  let enableSend = false
+
+  //Dispatches the input data to parent and resets input.
+  const sendMsg = () => {
+    dispatch('message', {
+      text: messageInput,
+      offChain: off_chain,
+      beam: activeBeam
+    })
+    //Reset input field
+    messageInput = ''
+    messageField.style.cssText = 'height:auto';
+    messageField.style.cssText = 'height: 40px';
+  }
+
+  //Checks if input is empty
+  $: {
+    enableSend = !!messageInput
+  }
+
+  $: {
+    if ($user.activeChat) {
+      off_chain = $webRTC.call.some((a) => a.chat == $user.activeChat.chat && a.connected)
     }
+  }
 
-    //This handles the emojis, lets fork the repo and make a darker theme.
-    function onEmoji(emoji) {
-        if (messageInput) {
-            messageInput += emoji
-        } else messageInput = emoji
-        openEmoji = false
+  $: if ($groups.replyTo.reply && $page.url.pathname === '/groups' && mount) {
+    fieldFocus()
+  }
+
+  $: if ($videoGrid.showChat && $videoGrid.showVideoGrid) {
+    fieldFocus()
+  }
+
+  async function fieldFocus() {
+    await sleep(200)
+    messageField.focus()
+  }
+
+  $: {
+    if ($beam.active.length) {
+      activeBeam = $beam.active.some(a => a.chat == $user.activeChat.chat && a.connected);
+      console.log("Beam active", activeBeam);
+    } else {
+      activeBeam = false
     }
+  }
 
-    //Input data to dispatch
-    let messageInput
-
-    //To handle button disabled enabled
-    let enableSend = false
-
-    //Dispatches the input data to parent and resets input.
-    const sendMsg = () => {
-        dispatch('message', {
-            text: messageInput,
-            offChain: off_chain,
-            beam: activeBeam
-        })
-        //Reset input field
-        messageInput = ''
-      messageField.style.cssText = 'height: 40px';
+  function autosize() {
+    console.log(messageField.scrollHeight)
+    if(messageInput !== '') {
+      messageField.style.cssText = 'height:auto';
+      messageField.style.cssText = 'height:' + messageField.scrollHeight + 'px';
     }
-
-    //Checks if input is empty
-    $: {
-        enableSend = !!messageInput
-    }
-
-    $: {
-        if ($user.activeChat) {
-            off_chain = $webRTC.call.some((a) => a.chat == $user.activeChat.chat && a.connected)
-        }
-    }
-
-    $: if ($groups.replyTo.reply && $page.url.pathname === '/groups' && mount) {
-        fieldFocus()
-    }
-
-    $: if ($videoGrid.showChat && $videoGrid.showVideoGrid) {
-        fieldFocus()
-    }
-
-    async function fieldFocus() {
-        await sleep(200)
-        messageField.focus()
-    }
-
-    $: {
-      if ($beam.active.length) {
-        activeBeam = $beam.active.some(a => a.chat == $user.activeChat.chat && a.connected);
-        console.log("Beam active", activeBeam);
-      } else {
-        activeBeam = false
-      }
-    }
-
-    function autosize(){
-      console.log(messageField.scrollHeight)
-        messageField.style.cssText = 'height:auto';
-        messageField.style.cssText = 'height:' + messageField.scrollHeight + 'px';
-    }
+  }
 
 </script>
 
@@ -116,7 +121,8 @@
 <div class="wrapper" class:rtc
      class:hide="{$boards.thisBoard == 'Home' && $page.url.pathname === '/boards'}"
      class:border-top="{$page.url.pathname !== '/boards'}">
-    <textarea rows="1" bind:this="{messageField}" bind:value="{messageInput}" on:click={() => openEmoji = false} autofocus></textarea>
+    <textarea rows="1" bind:this="{messageField}" bind:value="{messageInput}" on:click={() => openEmoji = false}
+              autofocus></textarea>
     <!--<EmojiSelector on:emoji={onEmoji} />-->
     <div style="position: relative; display: flex:">
         <div class:openEmoji={openEmoji} style="position: absolute; bottom: 3.45rem; right: 0; display: none">
@@ -158,7 +164,7 @@
     border: none;
     border-radius: 8px 0 0 8px;
     color: #ffffff;
-    display:block;
+    display: block;
     width: inherit;
     padding: 10px 15px;
     font-size: 14px;
@@ -217,6 +223,6 @@
   }
 
   .openEmoji {
-    display: flex!important;
+    display: flex !important;
   }
 </style>
