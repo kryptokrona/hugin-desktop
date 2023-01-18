bbed38bfc0309673b3e2bd962ea1bb8df58f48b2
n9lsjr <nilsjr@protonmail.com>
2023-01-18 18:51:48 +0100

    Added utils.js in backend

---
+++ src/backend/utils.cjs
@@ -0,0 +1,66 @@
+const nacl = require('tweetnacl')
+
+const hexToUint = (hexString) =>
+    new Uint8Array(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)))
+
+async function createGroup() {
+    return await Buffer.from(nacl.randomBytes(32)).toString('hex')
+}
+
+function toHex(str, hex) {
+    try {
+        hex = unescape(encodeURIComponent(str))
+            .split('')
+            .map(function (v) {
+                return v.charCodeAt(0).toString(16)
+            })
+            .join('')
+    } catch (e) {
+        hex = str
+        //console.log('invalid text input: ' + str)
+    }
+    return hex
+}
+
+function nonceFromTimestamp(tmstmp) {
+    let nonce = hexToUint(String(tmstmp))
+
+    while (nonce.length < nacl.box.nonceLength) {
+        let tmp_nonce = Array.from(nonce)
+
+        tmp_nonce.push(0)
+
+        nonce = Uint8Array.from(tmp_nonce)
+    }
+
+    return nonce
+}
+
+function fromHex(hex, str) {
+    try {
+        str = decodeURIComponent(hex.replace(/(..)/g, '%$1'))
+    } catch (e) {
+        str = hex
+        // console.log('invalid hex input: ' + hex)
+    }
+    return str
+}
+
+function trimExtra(extra) {
+    try {
+        let payload = fromHex(extra.substring(66))
+        let payload_json = JSON.parse(payload)
+        return fromHex(extra.substring(66))
+    } catch (e) {
+        return fromHex(Buffer.from(extra.substring(78)).toString())
+    }
+}
+
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+
+
+module.exports = {sleep, trimExtra, fromHex, nonceFromTimestamp, createGroup, hexToUint, toHex }
