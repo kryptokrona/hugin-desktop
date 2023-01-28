85f816d508ebee2d371e3fb757241c19fdd487dc
n9lsjr <nilsjr@protonmail.com>
2023-01-28 22:19:14 +0100

    Store update

--- src/lib/stores/files.js
+++ src/lib/stores/files.js
@@ -3,3 +3,11 @@ import { writable } from 'svelte/store'
 export const remoteFiles = writable([])
 
 export const localFiles = writable([])
+
+export const upload = writable([])
+
+export const download = writable([])
+
+export const fileSettings = writable({
+    showFiles: false,
+})
