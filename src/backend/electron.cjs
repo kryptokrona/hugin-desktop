9c57360afe9cdb99545d64e8059c7c86bd242293
n9lsjr <nilsjr@protonmail.com>
2024-04-02 16:06:45 +0200

    Open same window when clicking Hugin icon if minimized

--- src/backend/electron.cjs
+++ src/backend/electron.cjs
@@ -119,6 +119,21 @@ function createMainWindow() {
     else serveURL(mainWindow)
 }
 
+const gotTheLock = app.requestSingleInstanceLock()
+    
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+    
+}
+
 app.on('ready', createMainWindow)
 app.on('activate', () => {
     if (!mainWindow) {
