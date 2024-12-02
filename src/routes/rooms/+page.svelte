58f2cf735a4db62e24ed3ecc70d6072a8968cab6
n9lsjr <nilsjr@protonmail.com>
2024-12-02 04:39:34 +0100

    Add topic rule of no current room is found

--- src/routes/rooms/+page.svelte
+++ src/routes/rooms/+page.svelte
@@ -89,10 +89,11 @@ onMount(async () => {
         const file = isFile(data)
         if (file) data.file = file
         const thisroom = data.group === $rooms.thisRoom.key
+        const roomtopic = data.topic === $swarm.activeSwarm.topic
         const thistopic = data.file?.key === $rooms.thisRoom.topic
         const inrooms = $page.url.pathname === '/rooms'
         if (data.address === $user.myAddress) return
-            if ((thisroom || thistopic) && inrooms) {
+            if ((thisroom || thistopic || roomtopic) && inrooms) {
                 printRoomMessage(data)
             } else {
                 console.log("Another room")
