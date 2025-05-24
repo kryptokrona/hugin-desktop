bcbfd8a546166c6f92873eb7701a13e8569a4bdf
Nils Jr <nilsjr@protonmail.com>
2025-05-24 14:27:33 +0200

    Test update component with build

--- src/lib/components/chat/CodeBlock.svelte
+++ src/lib/components/chat/CodeBlock.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { run } from 'svelte/legacy';
 
     import Highlight, { HighlightAuto } from "svelte-highlight";
     import ashes from "svelte-highlight/styles/ashes";
@@ -30,15 +29,5 @@
       border-radius: 5px;
       font-family: "Montserrat";
     }
-
-    .code p {
-      font-family: "Montserrat";
-    }
-
-    .hljs {
-      white-space: pre-wrap;
-      border-radius: 5px !important;
-    }
-    
    
   </style>
