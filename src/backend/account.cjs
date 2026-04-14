e3931a7979df53c1d7f18ef47a96e452fc0a77a7
Nils Jr <nilsjr@protonmail.com>
2026-04-14 22:37:47 +0200

    Set media sharing in rooms to default

--- src/backend/account.cjs
+++ src/backend/account.cjs
@@ -1,316 +1,300 @@
-const { app, ipcMain, globalShortcut } = require('electron')
-const userDataDir = app.getPath('userData')
-const downloadDir = app.getPath('downloads')
-const { JSONFile, Low } = require('@commonify/lowdb')
-const { join } = require('path')
-const file = join(userDataDir, 'misc.db')
-const adapter = new JSONFile(file)
-const db = new Low(adapter)
-const dbPath = userDataDir + '/SQLmessages.db'
-const { getGroups, getRooms, loadBlockList, loadKeys, loadDB, saveRoomUser, loadRoomUsers } = require('./database.cjs')
-const fs = require('fs')
-const Store = require('electron-store')
-const { hash } = require('crypto')
-const { toBrowbroserKey } = require('./utils.cjs')
-const store = new Store()
-const { uIOhook } = require('uiohook-napi')
+const { app, ipcMain, globalShortcut } = require('electron');
+const userDataDir = app.getPath('userData');
+const downloadDir = app.getPath('downloads');
+const { JSONFile, Low } = require('@commonify/lowdb');
+const { join } = require('path');
+const file = join(userDataDir, 'misc.db');
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+const dbPath = userDataDir + '/SQLmessages.db';
+const {
+	getGroups,
+	getRooms,
+	loadBlockList,
+	loadKeys,
+	loadDB,
+	saveRoomUser,
+	loadRoomUsers
+} = require('./database.cjs');
+const fs = require('fs');
+const Store = require('electron-store');
+const { hash } = require('crypto');
+const { toBrowbroserKey } = require('./utils.cjs');
+const store = new Store();
+const { uIOhook } = require('uiohook-napi');
 
 ipcMain.on('set-avatar', (e, data) => {
-  const avatar = Buffer.from(data).toString('base64')
-  store.set({
-    avatar: avatar
-  })
-  Hugin.avatar = avatar
-})
-
+	const avatar = Buffer.from(data).toString('base64');
+	store.set({
+		avatar: avatar
+	});
+	Hugin.avatar = avatar;
+});
 
 ipcMain.on('save-room-user', (e, data) => {
-  saveRoomUser(data)
-})
+	saveRoomUser(data);
+});
 
 ipcMain.handle('get-room-users', async (e, key) => {
-  if (!key) return []
-  return await get_room_users(key)
-})
+	if (!key) return [];
+	return await get_room_users(key);
+});
 
 async function get_room_users(key) {
-  return await loadRoomUsers(key)
-};
-
-ipcMain.handle('get-avatar', () => {
-  return get_avatar()
-})
-
-ipcMain.handle('get-hugin-node', async () => { 
-  if (Object.keys(Hugin.huginNode).length === 0) {
-    Hugin.huginNode = store.get('huginNode') ?? { address: '', pub: true}
-  }
-  return Hugin.huginNode 
-})
-
-const get_avatar = () => {
-  return store.get('avatar') ?? ""
+	return await loadRoomUsers(key);
 }
 
-ipcMain.on('change-download-dir', (e, dir) => {
-  store.set({
-    download: {
-      dir: dir
-    }
-  })
-  Hugin.downloadDir = dir
-})
+ipcMain.handle('get-avatar', () => {
+	return get_avatar();
+});
 
+ipcMain.handle('get-hugin-node', async () => {
+	if (Object.keys(Hugin.huginNode).length === 0) {
+		Hugin.huginNode = store.get('huginNode') ?? { address: '', pub: true };
+	}
+	return Hugin.huginNode;
+});
+
+const get_avatar = () => {
+	return store.get('avatar') ?? '';
+};
+
+ipcMain.on('change-download-dir', (e, dir) => {
+	store.set({
+		download: {
+			dir: dir
+		}
+	});
+	Hugin.downloadDir = dir;
+});
 
 ipcMain.on('change-idle-time', (e, time) => {
-  store.set({
-    idle: {
-      time: time
-    }
-  })
-})
+	store.set({
+		idle: {
+			time: time
+		}
+	});
+});
 
 //Set banned list for UI things.
 ipcMain.on('room-banned', (e, key) => {
-  let list = store.get('banned') ?? []
-  list.push(key)
-  store.set({
-    banned: list
-  })
-})
+	let list = store.get('banned') ?? [];
+	list.push(key);
+	store.set({
+		banned: list
+	});
+});
 
 ipcMain.on('group-notifications', (e, list) => {
-  store.set({
-    off: {
-      notifications: list
-    }
-  })
-})
+	store.set({
+		off: {
+			notifications: list
+		}
+	});
+});
 
 ipcMain.on('set-nickname', (e, name) => {
-  Hugin.nickname = name
-})
-
+	Hugin.nickname = name;
+});
 
 ipcMain.on('push-to-talk', (e, setting) => {
+	store.set({
+		pushToTalk: {
+			key: setting.key,
+			on: setting.on,
+			name: setting.name
+		}
+	});
 
-  store.set({
-    pushToTalk: {
-      key: setting.key,
-      on: setting.on,
-      name: setting.name
-    }
-  })
+	if (setting.on) {
+		if (process.platform === 'darwin') {
+			//Perms to access push to talk.
+			const { askForAccessibilityAccess } = require('node-mac-permissions');
+			askForAccessibilityAccess();
+		}
 
-  if (setting.on) {
-
-    if (process.platform === 'darwin') {
-      //Perms to access push to talk.
-      const { askForAccessibilityAccess } = require('node-mac-permissions')
-      askForAccessibilityAccess()
-    }
-
-    push_to_talk(setting)
-  } else {
-    uIOhook.stop();
-  }
-})
-
+		push_to_talk(setting);
+	} else {
+		uIOhook.stop();
+	}
+});
 
 ipcMain.on('sounds', (e, setting) => {
-
-  store.set({
-    sounds: setting
-  })
-
-})
-
+	store.set({
+		sounds: setting
+	});
+});
 
 ipcMain.on('voice-activation', (e, setting) => {
-
-  store.set({
-    voiceActivation: {
-      enabled: setting.enabled,
-      sensitivity: setting.sensitivity
-    }
-  })
-
-})
+	store.set({
+		voiceActivation: {
+			enabled: setting.enabled,
+			sensitivity: setting.sensitivity
+		}
+	});
+});
 
 class Account {
-  constructor() {
+	constructor() {
+		this.known_keys = [];
+		this.block_list = [];
+		this.walletName = [];
+		this.node = {};
+		this.wallet = {};
+		this.sender = null;
+		this.downloadDir = '';
+		this.nickname = '';
+		this.address = '';
+		this.roomFiles = [];
+		this.avatar = '';
+		this.syncImages = null;
+		this.huginNode = {};
+		this.talkKey = null;
+	}
 
-    this.known_keys = []
-    this.block_list = []
-    this.walletName = []
-    this.node = {}
-    this.wallet = {}
-    this.sender = null
-    this.downloadDir = ""
-    this.nickname = ""
-    this.address = ""
-    this.roomFiles = []
-    this.avatar = ""
-    this.syncImages = null
-    this.huginNode = {}
-    this.talkKey = null
+	async init(wallet, name, node, s) {
+		this.wallet = wallet;
+		this.walletName = name;
+		this.sender = s;
+		this.node = node;
+		this.downloadDir = store.get('download.dir') ?? downloadDir;
+		this.address = wallet.getPrimaryAddress();
+		this.avatar = get_avatar();
+		this.syncImages = store.get('syncImages') ?? [];
+		this.huginNode = store.get('huginNode') ?? { address: '', pub: true };
+		console.log('HuginNode', this.huginNode);
+		if (!store.get('pool.checked')) {
+			//If no value is set, check from 24h back on first check.
+			store.set({
+				pool: {
+					checked: Math.floor(Date.now() / 1000 - 84600)
+				}
+			});
+		}
 
-  }
+		await this.load();
+		this.ipc();
+	}
 
-  async init(wallet, name, node, s) {
-    this.wallet = wallet
-    this.walletName = name
-    this.sender = s
-    this.node = node
-    this.downloadDir = store.get('download.dir') ?? downloadDir
-    this.address = wallet.getPrimaryAddress()
-    this.avatar = get_avatar()
-    const savedSync = store.get('syncImages')
-    this.syncImages = savedSync ?? []
-    this._syncDefault = savedSync === undefined
-    this.huginNode = store.get('huginNode') ?? { address: '', pub: true}
-    console.log("HuginNode", this.huginNode);
-    if (!store.get('pool.checked')) {
-      //If no value is set, check from 24h back on first check.
-      store.set({
-        pool: {
-          checked: Math.floor((Date.now() / 1000) - 84600)
-        }
-      })
-    }
+	async load() {
+		await loadDB(userDataDir, dbPath, this.wallet.getPrimaryAddressPrivateKeys());
+		const [my_contacts, keys] = await loadKeys(true);
+		const my_groups = await getGroups();
+		const block_list = await loadBlockList();
+		const deleteAfter = store.get('delete.after');
+		const idle = store.get('idle.time') ?? 300;
+		const notifications = store.get('off.notifications') ?? [];
+		const banned = store.get('banned') ?? [];
+		const usersBanned = store.get('bannedUsers') ?? [];
+		const files = store.get('files') ?? [];
+		const avatars = [];
+		const pushToTalk = store.get('pushToTalk') ?? { key: null, on: false, name: '' };
+		const sounds = store.get('sounds') ?? true;
+		const voiceActivation = store.get('voiceActivation') ?? { enabled: true, sensitivity: 160 };
 
-    await this.load()
-    this.ipc()
-  }
+		const my_rooms = await getRooms();
+		const allTopics = [...my_groups, ...my_rooms].map((g) => g.topic || g.key).filter(Boolean);
+		const newTopics = allTopics.filter((t) => !this.syncImages.includes(t));
+		if (newTopics.length > 0) {
+			this.syncImages = [...this.syncImages, ...newTopics];
+			store.set({ syncImages: this.syncImages });
+		}
 
-  async load() {
-    await loadDB(userDataDir, dbPath, this.wallet.getPrimaryAddressPrivateKeys())
-    const [my_contacts, keys] = await loadKeys((true))
-    const my_groups = await getGroups()
-    const block_list = await loadBlockList()
-    const deleteAfter = store.get('delete.after')
-    const idle = store.get('idle.time') ?? 300
-    const notifications = store.get('off.notifications') ?? []
-    const banned = store.get('banned') ?? []
-    const usersBanned = store.get('bannedUsers') ?? []
-    const files = store.get('files') ?? []
-    const avatars = []
-    const pushToTalk = store.get('pushToTalk') ?? { key: null, on: false, name: '' }
-    const sounds = store.get('sounds') ?? true
-    const voiceActivation = store.get('voiceActivation') ?? { enabled: true, sensitivity: 160 }
+		if (pushToTalk.on) {
+			push_to_talk(pushToTalk);
+		}
 
-    if (this._syncDefault) {
-      const my_rooms = await getRooms()
-      const allTopics = [...my_groups, ...my_rooms].map(g => g.topic || g.key).filter(Boolean)
-      if (allTopics.length > 0) {
-        this.syncImages = allTopics
-        store.set({ syncImages: this.syncImages })
-      }
-    }
+		this.sender('wallet-started', [
+			this.node,
+			my_groups.reverse(),
+			block_list,
+			my_contacts.reverse(),
+			deleteAfter,
+			this.downloadDir,
+			Buffer.from(this.avatar, 'base64'),
+			idle,
+			notifications,
+			banned,
+			files,
+			avatars,
+			this.syncImages,
+			pushToTalk,
+			sounds,
+			voiceActivation
+		]);
 
-    if (pushToTalk.on) {
-      push_to_talk(pushToTalk)
-    }
+		this.known_keys = keys;
+		this.block_list = block_list;
+	}
 
+	send(channel, send) {
+		this.sender(channel, send);
+	}
 
-    this.sender('wallet-started', [
-      this.node,
-      my_groups.reverse(),
-      block_list,
-      my_contacts.reverse(),
-      deleteAfter,
-      this.downloadDir,
-      Buffer.from(this.avatar, 'base64'),
-      idle,
-      notifications,
-      banned,
-      files,
-      avatars,
-      this.syncImages,
-      pushToTalk,
-      sounds,
-      voiceActivation
-    ])
+	ban(address, topic) {
+		if (this.banned(address, topic)) return;
+		list.push({ address, topic });
+		store.set({
+			bannedUsers: list
+		});
+	}
 
-    this.known_keys = keys
-    this.block_list = block_list
-  }
+	banned(address, topic) {
+		const banned = store.get('bannedUsers') ?? [];
+		return banned.some((a) => a.address === address && a.topic === topic);
+	}
 
-  send(channel, send) {
+	blocked(address) {
+		return this.block_list.some((a) => a.address === address);
+	}
 
-    this.sender(channel, send)
-  }
+	file_info(file) {
+		const list = this.get_files();
+		list.push(file);
+		store.set({
+			files: list
+		});
+		this.sender('files', list);
+	}
 
-  ban(address, topic) {
-    if (this.banned(address, topic)) return
-    list.push({ address, topic })
-    store.set({
-      bannedUsers: list
-    })
-  }
+	get_files() {
+		return store.get('files') ?? [];
+	}
 
-  banned(address, topic) {
-    const banned = store.get('bannedUsers') ?? []
-    return banned.some(a => a.address === address && a.topic === topic)
-  }
+	ipc() {
+		ipcMain.on('sync-images', (e, list) => {
+			store.set({
+				syncImages: list
+			});
+			this.syncImages = list;
 
-  blocked(address) {
-    return this.block_list.some(a => a.address === address)
-  }
-
-  file_info(file) {
-    const list = this.get_files()
-    list.push(file)
-    store.set({
-      files: list
-    })
-    this.sender('files', list)
-  }
-
-  get_files() {
-    return store.get('files') ?? []
-  }
-
-  ipc() {
-
-    ipcMain.on('sync-images', (e, list) => {
-      store.set({
-        syncImages: list
-      })
-      this.syncImages = list
-
-      console.log("Set sync image setting", list)
-    })
-
-  }
-
+			console.log('Set sync image setting', list);
+		});
+	}
 }
 
 function push_to_talk(pushToTalk) {
+	//PUSH TO TALK keydown!
+	Hugin.talkKey = pushToTalk.key;
+	uIOhook.on('keydown', (e) => {
+		const code = toBrowbroserKey(e.keycode);
+		if (code === Hugin.talkKey) {
+			Hugin.send('key-event', { state: 'DOWN', keyCode: code });
+		}
+	});
 
-  //PUSH TO TALK keydown!
-  Hugin.talkKey = pushToTalk.key
-  uIOhook.on('keydown', (e) => {
-    const code = toBrowbroserKey(e.keycode)
-    if (code === Hugin.talkKey) {
-      Hugin.send('key-event', { state: 'DOWN', keyCode: code })
-    }
-  })
-
-  //PUSH TO TALK keyup!
-  uIOhook.on('keyup', (e) => {
-    const code = toBrowbroserKey(e.keycode)
-    console.log("key up")
-    if (code === Hugin.talkKey) {
-      Hugin.send('key-event', { state: 'UP', keyCode: code })
-    }
-
-  })
-  //Starts the listener
-  uIOhook.start()
+	//PUSH TO TALK keyup!
+	uIOhook.on('keyup', (e) => {
+		const code = toBrowbroserKey(e.keycode);
+		console.log('key up');
+		if (code === Hugin.talkKey) {
+			Hugin.send('key-event', { state: 'UP', keyCode: code });
+		}
+	});
+	//Starts the listener
+	uIOhook.start();
 }
 
-let Hugin = new Account()
+let Hugin = new Account();
 
-
-module.exports = { Hugin }
+module.exports = { Hugin };
